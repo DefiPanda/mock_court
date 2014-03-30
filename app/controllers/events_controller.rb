5885ff48a50e00552b22dc39e8ff2d036c975c49
jw2013 <zhew@live.unc.edu>
2014-03-30 12:49:42 -0400

    require login on homepage

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_filter :authenticate_user!, :except => [:index, :show]
+  before_filter :authenticate_user!
 
 	def index
       @event = Event.all
